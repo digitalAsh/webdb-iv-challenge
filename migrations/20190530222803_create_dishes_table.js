c3ba7872de57a548eb650e35f2a98ce1a35c1930
jal9626 <jal9626@gmail.com>
2019-05-30 23:50:38 -0400

    migration typo fixed

--- migrations/20190530222803_create_dishes_table.js
+++ migrations/20190530222803_create_dishes_table.js
@@ -7,9 +7,9 @@ exports.up = async function(knex) {
 
     await knex.schema.createTable('recipes', (tbl) => {
         tbl.increments('id')
-        tbl.string('name')
+        tbl.string('name').unique().notNullable()
         tbl.integer('dish_id').references('id').inTable('dishes').onDelete('CASCADE')
-        .onUpdate('CASCADE').notNullabe()
+        .onUpdate('CASCADE').notNullable()
         tbl.string('instructions')
     })
 
@@ -22,13 +22,13 @@ exports.up = async function(knex) {
     await knex.schema.createTable('recipes_ingredients', (tbl) => {
         tbl.increments('id')
         tbl.integer('recipe_id').references('id').inTable('recipes').onDelete('CASCADE')
-        .onUpdate('CASCADE').notNullabe()
+        .onUpdate('CASCADE').notNullable()
         tbl.integer('ingredient_id').references('id').inTable('ingredients').onDelete('CASCADE')
-        .onUpdate('CASCADE').notNullabe()
+        .onUpdate('CASCADE').notNullable()
     })
 };
 
-exports.down = function(knex) {
+exports.down = async function(knex) {
   await knex.schema.dropTableIfExists('recipes_ingredients')
   await knex.schema.dropTableIfExists('ingredients')
   await knex.schema.dropTableIfExists('recipes')
